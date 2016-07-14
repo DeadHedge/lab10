4962aca80083810987b2e3c4a67d345d84d862d8
Josh Teo <joshua.teoleekuok@gmail.com>
2016-07-14 15:21:05 -0400

    modified

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,37 @@
-alert("Hi, it works!");
+// var submit = function() {
+// 	var firsName =document.getElementById("firstName");
+// 	var lastName = document.getElementById("lastName");
+// 	var date = document.getElementById("date");
+// 	var email = document.getElementById("email");
+// 	var seat = document.getElementById("selectedSeat");
+// };
+
+
+
+// function Seat(seatNum, firstName, lastName, date, email) {
+// 	this.seatNum = seatNum;
+// 	this.firstName = firstName;
+// 	this.lastName = lastName;
+// }
+// console.log(submit);
+
+// var seat1 = new Seat(John, beckius, 9, @hotmail) {
+// 	seat1.firstName = John
+// };
+// var arr = [];
+// arr[textAreaseatnum.value].firstName = 
+
+//  textArea.value
+// 	for(var i = 0; i < 24 ; i++) {
+// 		var newSeat = new Seat(i);
+// 		arr.push(newSeat);
+// 	}
+
+
+// seat1 = {
+// 	firstName: John,
+// 	lastName: beckius,
+// 	date: 9,
+// 	email: @hotmail
+// }
+
