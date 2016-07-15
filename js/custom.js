0ac023409c5450408ce782ead8205778ec0b0e8f
John Beckius <jbeckius91@gmail.com>
2016-07-15 10:53:07 -0400

    built js onclick functionality.

--- js/custom.js
+++ js/custom.js
@@ -83,22 +83,65 @@ $(document).ready(function(){
 });
 
 //Seat constructor function
-function Seat() {
+function Seat(id) {
   this.taken = false;
+  this.id = id;
+  this.selected = false;
 }
 
-
-
-
 seatArray = [];
+seatsSelected = [];
 
 for (var i = 0; i < 24 ; i++){
-  var newSeat = new Seat();
+  var newSeat = new Seat(i + 1);
   seatArray.push(newSeat);
 }
 
+// add event listener for all grid elements
+$('.seat').on("click", function() {
+  var $id = $(this).siblings('p').text();
+  var id = parseInt($id);
+  if(seatArray[id - 1].taken === true) {
+    console.log("That seat is taken!");
+    return;
+  }
+  if(seatArray[id - 1].selected === true) {
+    seatArray[id - 1].selected = false
+  } else {seatArray[id - 1].selected = true}
+  var seatListContent = "";
+  seatArray.forEach(function(each) {
+    if (each.selected === true) {
+      console.log("seat list" + each.id.toString());
+      seatListContent += (each.id.toString() + " ");
+      console.log(seatListContent);
+    }
+  })
+  $('#seatList').text(seatListContent)
+
+  console.log(seatArray[id - 1].selected);
+
+  console.log(typeof(id));
+  console.log(id);
+
+
+});
+
+
+
+//take a string of comma-seperated integers
+
+//return an array of integers
+
 function onSubmit() {
   // console.log("checking event listener");
+
+  // on submit
+  // check each object for selected === true
+
+  // if selected === true, set seat as reserved and save user info
+
+
+
   //get seat number
   var $seatNum = $('#selectedSeat');
   console.log($seatNum);
@@ -106,26 +149,32 @@ function onSubmit() {
   var $lastName = $('#lastName');
   var $email = $('#email');
 
+  seatArray.forEach(function (each) {
+    if(each.selected === true) {
+      console.log(each.id + " is selected.");
+      each.firstName = $firstName.val();
+      each.lastName = $lastName.val();
+      each.email = $email.val();
+      //set taken to true
+      each.taken = true;
+    }
+    console.log(each);
+    })
   //find object based on seat number
   console.log(seatArray);
   var seatObject = seatArray[$seatNum.val() - 1];
   console.log(seatObject.taken);
-  //check if seat is taken
-  if (seatObject.taken === true) {
-    console.log("You too late!!");
-    return;
-  }
+  //check if seat is taken. If so, send message.
+  // if (seatObject.taken === true) {
+  //   console.log("You too late!!");
+  //   return;
+  // }
   console.log($seatNum.val());
   console.log(seatObject);
 
-  // if seat taken = false, send error message
 
   // set object properties to equal other form values
-  seatObject.firstName = $firstName.val();
-  seatObject.lastName = $lastName.val();
-  seatObject.email = $email.val();
-  //set taken to true
-  seatObject.taken = true;
+
   console.log(seatObject.taken);
   console.log(seatArray);
 }
